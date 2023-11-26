0e16f94c3292c44352bea86eddd4ad9fdc410431
salahelfatimi <salahfatimi16@gmail.com>
2023-11-27 00:16:04 +0100

    add

---
+++ src/pages/components/Home/descriptionInnovtech.jsx
@@ -0,0 +1,24 @@
+export default function DescriptionInnovtech(){
+    return(
+        <>
+        <div className="flex flex-col items-center gap-4  relative z-10   ">
+            <span className="lg:text-4xl text-2xl text-[#3A9BE1] text-center  font-bold">
+              Formation Doctorale InnovTech{" "}
+            </span>
+            <span className="text-[#848789] lg:font-medium  text-center lg:text-lg ">
+              Le projet InnovTech vise à regrouper et collaborer les
+              laboratoires de l&apos;UCA pour répondre à ses orientations
+              stratégiques. Cette mutualisation crée une synergie entre
+              chercheurs, indépendamment de leur affiliation. Cette approche met
+              en avant des axes de recherche grâce à des équipes de taille
+              compétitive nationale et internationale. InnovTech réunit 14
+              laboratoires et 5 équipes de recherche de 6 établissements de
+              l&apos;UCA, répartis dans 3 villes (Marrakech, Safi, Essaouira).
+              Leur complémentarité vise à établir un pôle de référence en
+              innovation technologique, à l&apos;échelle nationale et
+              internationale.
+            </span>
+        </div>
+        </>
+    )
+}
---
+++ src/pages/components/Home/infoBelkhayat.jsx
@@ -0,0 +1,49 @@
+export default function InfoBelkhayat(){
+    return(
+        <>
+            <div className=" container flex flex-col lg:flex-row items-center gap-12 ">
+                <img className=" rounded-md lg:w-[50%]" src="/image/home/Belkhayat.png" alt="" />
+                <div className="flex flex-col  lg:items-start items-center text-center lg:text-start gap-4">
+                    <span className=" text-2xl lg:text-3xl font-bold text-[#1A60A1]">M. Belkhayat Driss</span>
+                    <span className=" text-lg lg:text-xl font-medium">Le directeur de la cité de l&apos;innovation de Marrakech</span>
+                    <div className="flex gap-4 items-center">
+                        <span>
+                            <svg width="50" height="50" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none">
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path fill="#0A66C2" d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"></path>
+                            </g>
+                            </svg>
+                        </span>
+
+                        <span>
+                            <svg height="45" width="45" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path fill="#E6F3FF" d="M512,105.739v300.522c0,27.715-22.372,50.087-50.087,50.087H50.087C22.372,456.348,0,433.976,0,406.261V105.739c0-0.89,0-1.781,0.111-2.671c1.336-25.6,21.704-45.969,47.304-47.304c0.89-0.111,1.781-0.111,2.671-0.111h411.826c0.89,0,1.892,0,2.783,0.111c25.489,1.336,45.857,21.704,47.193,47.193C512,103.847,512,104.849,512,105.739z"></path>
+                                <path fill="#CFDBE6" d="M464.696,55.763c-0.892-0.111-1.891-0.111-2.783-0.111H256v400.696h205.913c27.715,0,50.087-22.372,50.087-50.087V105.739c0-0.89,0-1.892-0.111-2.783C510.553,77.468,490.184,57.099,464.696,55.763z"></path>
+                                <path fill="#FF4B26" d="M511.889,102.957c-1.336-25.489-21.704-45.857-47.193-47.193C382.89,137.569,336.951,183.509,256,264.459C225.291,233.732,77.61,85.958,47.416,55.763c-25.6,1.336-45.969,21.704-47.304,47.304C0,103.958,0,104.849,0,105.739v300.522c0,27.715,22.372,50.087,50.087,50.087h16.696V169.739l165.621,165.51c6.456,6.567,15.026,9.795,23.597,9.795c8.57,0,17.141-3.228,23.597-9.795l165.621-165.621v286.72h16.696c27.715,0,50.087-22.372,50.087-50.087V105.739C512,104.849,512,103.847,511.889,102.957z"></path>
+                                <path fill="#D93F21" d="M279.596,335.249l165.621-165.621v286.72h16.696c27.715,0,50.087-22.372,50.087-50.087V105.739c0-0.89,0-1.892-0.111-2.783c-1.336-25.489-21.704-45.857-47.193-47.193C382.891,137.569,336.951,183.509,256,264.459v80.584C264.57,345.043,273.141,341.816,279.596,335.249z"></path>
+                            </g>
+                            </svg>
+                        </span>
+                        <span>
+                            <svg height="45" width="45" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none">
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path fill="#1877F2" d="M15 8a7 7 0 00-7-7 7 7 0 00-1.094 13.915v-4.892H5.13V8h1.777V6.458c0-1.754 1.045-2.724 2.644-2.724.766 0 1.567.137 1.567.137v1.723h-.883c-.87 0-1.14.54-1.14 1.093V8h1.941l-.31 2.023H9.094v4.892A7.001 7.001 0 0015 8z"></path>
+                                    <path fill="#ffffff" d="M10.725 10.023L11.035 8H9.094V6.687c0-.553.27-1.093 1.14-1.093h.883V3.87s-.801-.137-1.567-.137c-1.6 0-2.644.97-2.644 2.724V8H5.13v2.023h1.777v4.892a7.037 7.037 0 002.188 0v-4.892h1.63z"></path>
+                                </g>
+                            </svg>
+                        </span>
+                    </div>
+
+
+                </div>
+            </div>
+        </>
+    )
+}
