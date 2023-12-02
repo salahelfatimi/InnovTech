18dc03ac39ecb0357b0b7e2e336cfc7cfb93ebe4
salahelfatimi <salahfatimi16@gmail.com>
2023-12-02 01:34:00 +0100

    add

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,13 +9,13 @@ export default function Layout({ children }) {
   useEffect(() => {
     const slideInterval = setInterval(() => {
       setShow(true);
-    }, 5000);
+    }, 3000);
 
     // Clear the interval after 30 seconds
     setTimeout(() => {
       clearInterval(slideInterval);
       setShow(true);
-    }, 5000);
+    }, 3000);
 
     // Clean up the interval when the component unmounts
     return () => clearInterval(slideInterval);
