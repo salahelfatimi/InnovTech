42507224d4ba84ae6045dd9ead61120c47769578
salahelfatimi <salahfatimi16@gmail.com>
2023-12-13 20:32:59 +0100

    add

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar(){
                             <div className="flex gap-4 items-center ">
                           
                                 
-                                <img src={`/image/${theme=== "dark"?'logoDark':'logo'}.gif`} alt="logo" className="w-48 " />
+                                <img src={`/image/${theme=== "dark"?'logoDark':'logo'}.gif`} alt="logo" className="w-36 " />
                             </div>
                             <div className="flex flex-row items-center gap-4">
                                 {itemsNav.map((ele, index) => (
@@ -68,7 +68,7 @@ export default function Navbar(){
                                 <div className="flex px-2  items-center justify-between">
                                 <div className="flex gap-4 items-center ">
                           
-                                <img src={`/image/${theme=== "dark"?'logoDark':'logo'}.gif`} alt="logo" className="w-40 " />
+                                <img src={`/image/${theme=== "dark"?'logoDark':'logo'}.gif`} alt="logo" className="w-32 " />
                                 </div>
                                     <button onClick={()=>{setOpen(!open)}} className=" md:hidden block">
                                             <Menu size={30} className=" stroke-2"/>
