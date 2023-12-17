c0741e1424b7c504ac22687767fb7ea947c83e23
salahelfatimi <salahfatimi16@gmail.com>
2023-12-17 13:17:56 +0100

    machange logo loading

--- src/pages/components/loading.jsx
+++ src/pages/components/loading.jsx
@@ -7,13 +7,13 @@ export default function Loading() {
   useEffect(() => {
     const slideInterval = setInterval(() => {
       setShow(true);
-    }, 1000);
+    }, 2000);
 
     // Clear the interval after 30 seconds
     setTimeout(() => {
       clearInterval(slideInterval);
       setShow(true);
-    }, 1000);
+    }, 2000);
 
     // Clean up the interval when the component unmounts
     return () => clearInterval(slideInterval);
@@ -21,13 +21,17 @@ export default function Loading() {
   return (
     <>
       <div
-        className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc] dark:bg-[#2d2e32] duration-1000 transition z-50  ${
+        className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc] dark:bg-[#272B34] duration-1000 transition z-50  ${
           show == false ? "translate-x-0" : "-translate-y-full"
         } `}
       >
-        <span className={`font-extrabold text-4xl animate-bounce flex justify-center  `}>
-        <img src="/image/logo/logoInnovtechDark.gif" alt="logo" className="w-1/3 dark:block hidden " />
-        <img src="/image/logo/logoInnovtech.gif" alt="logo" className="w-1/3 block dark:hidden " />
+        <span className={`font-extrabold md:text-5xl text-4xl animate-bounce flex justify-center  `}>
+        <span className=" flex items-center  ">
+          
+          Innov<span className="text-[#ff9825]">TECH</span>
+        </span>
+        {/* <img src="/image/logo/logoInnovtechDark.gif" alt="logo" className="w-1/3 dark:block hidden " />
+        <img src="/image/logo/logoInnovtech.gif" alt="logo" className="w-1/3 block dark:hidden " /> */}
         </span>
       </div>
     </>
