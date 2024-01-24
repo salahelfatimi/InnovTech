38fd79fc40b2969cc959194ba83eecfdf824f326
salahelfatimi <salahfatimi16@gmail.com>
2024-01-24 13:09:13 +0100

    update icon dark

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 
 import { useTheme } from "next-themes";
 import { useRouter } from 'next/router'
-import { Codesandbox, Menu , X} from "react-feather";
+import { Codesandbox, Menu , Moon, Sun, X} from "react-feather";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 export default function Navbar(){
@@ -12,7 +12,7 @@ export default function Navbar(){
     const itemsNav=[
         {title:"Accueil",href:"/"},
         // {title:"laboratoire",href:"/laboratoire"},
-        {title:"Etablissment",href:"/etablissment"},
+        {title:"Etablissments",href:"/etablissment"},
         {title:"Contact",href:"/contact"},
         {title:"à propos",href:"/aboute"}
     ]
@@ -48,10 +48,8 @@ export default function Navbar(){
                                 </Link>
                                 ))}
                                 <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="focus:outline-none">
-                                <svg className={`transition duration-700 ${theme === "dark" ? "rotate-180" : "rotate-0"}`} width="30px" height="30px" viewBox="0 0 24 24" fill={theme === "light" ? "#000" : "#fff"} xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill={theme === "light" ? "#000" : "#fff"} />
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill={theme === "light" ? "#000" : "#fff"} />
-                                </svg>
+                                    <Sun size={30} className={`${theme === "dark" ? " block" : " hidden"}`}/>
+                                    <Moon size={30} className={`  ${theme === "dark" ? "hidden" : " block"}`}/>
                                 </button>
                             </div>
                         </div>
@@ -90,10 +88,10 @@ export default function Navbar(){
                                                 </Link>
                                             ))}
                                             <button onClick={()=>{ setTheme(theme === "dark" ? "light" : "dark"),setOpen(!open)}}>
-                                                <svg  className={`transition duration-700  ${theme==="dark"?"rotate-180":"rotate-0"}`}  width="50px" height="50px" viewBox="0 0 24 24" fill={theme === "light" ? "#000" : "#fff"} xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill={theme === "light" ? "#000" : "#fff"}/>
-                                                    <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill={theme === "light" ? "#000" : "#fff"}/>
-                                                </svg>
+                                            
+                                                <Sun size={50} className={`${theme === "dark" ? " block" : " hidden"}`}/>
+                                                <Moon size={50} className={`  ${theme === "dark" ? "hidden" : " block"}`}/>
+                                           
                                             </button>
                                                    
                             
